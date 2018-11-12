2a1ae2ab2649645c7d38f6dcd64ca262c54e322a
kaziHoque <43328051+kaziHoque@users.noreply.github.com>
2018-11-12 12:52:46 +0600

    Add files via upload

--- workspace/server.js
+++ workspace/server.js
@@ -1,15 +1,57 @@
+//  var http = require('http');
+//  var fs = require('fs');
+ 
+//   var server = http.createServer(function(req, res)
+//   {
+//     res.statusCode = 200;
+//     // res.setHeader('Content-Type', 'text/plain');
+//     // res.end("Hello World\n");
+    
+//         res.setHeader('Content-Type', 'text/html');
+//         fs.readFile('index.html', function(err, data){
+        
+//         if(err)
+//         {
+//             return console.log('File Read Error!');
+//         }
+        
+//         res.end(data);
+        
+//         });
+        
+//   });
+//   server.listen(process.env.PORT, process.env.IP, function(){
+//     console.log('Server running');
+//   });
+
+
+
+
+
+
 var http = require('http');
-var fs=require('fs');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    fs.readFile('index.html', function(err,data){
-        if (err){
-            return console.log("File read error");
-        }
-    })
- res.end("Hello World\n");
-  });
-  server.listen(process.env.PORT, process.env.IP, function(){
+var express = require('express');
+var app = express();
+var server = http.Server(app);
+
+app.get('/', function(request, response){
+    response.sendFile(__dirname+'/index.html');
+    
+
+    
+});
+
+
+
+
+app.get('/about', function(request, response){
+    response.sendFile(__dirname+'/about.html');
+    
+
+    
+});
+
+
+ server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
