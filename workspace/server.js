19008e3ed67e53c433631dde771a562901abbff6
kaziHoque <43328051+kaziHoque@users.noreply.github.com>
2018-11-14 10:40:46 +0600

    Add files via upload

--- workspace/server.js
+++ workspace/server.js
@@ -1,38 +1,13 @@
-//  var http = require('http');
-//  var fs = require('fs');
- 
-//   var server = http.createServer(function(req, res)
-//   {
-//     res.statusCode = 200;
-//     // res.setHeader('Content-Type', 'text/plain');
-//     // res.end("Hello World\n");
-    
-//         res.setHeader('Content-Type', 'text/html');
-//         fs.readFile('index.html', function(err, data){
-        
-//         if(err)
-//         {
-//             return console.log('File Read Error!');
-//         }
-        
-//         res.end(data);
-        
-//         });
-        
-//   });
-//   server.listen(process.env.PORT, process.env.IP, function(){
-//     console.log('Server running');
-//   });
-
-
-
-
-
-
 var http = require('http');
 var express = require('express');
 var app = express();
 var server = http.Server(app);
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({encoded:true}));
+
 
 app.get('/', function(request, response){
     response.sendFile(__dirname+'/index.html');
@@ -52,6 +27,25 @@ app.get('/about', function(request, response){
 });
 
 
+ app.get('/new-article', function(request, response)
+  {
+      response.sendFile(__dirname+'/form.html');
+  });
+  
+  
+  
+  app.post('/api/article/create', function(request, response)
+  {
+      console.log(request.body);
+      if(!request.body.title){
+         
+          return response.status(400).json({error: 'PLease add a title!'});
+      }
+      response.status(200).json({message: 'Article Created!'});
+  });
+  
+
  server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
+    
   });
